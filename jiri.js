7811c0787db8e313d0e3db4276fc0f8d106a9369
Stacy Acker <stacyacker@Stacys-MacBook-Pro.local>
2019-01-13 20:29:07 -0500

    project started

---
+++ jiri.js
@@ -0,0 +1,20 @@
+  /* Load the HTTP library */
+  var http = require("http");
+
+ 
+
+var Spotify = require('node-spotify-api');
+ 
+
+var spotify = new Spotify({
+    id: <your spotify client id>,
+    secret: <your spotify client secret>
+  });
+   
+  spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+   
+  console.log(data); node install
+  });
